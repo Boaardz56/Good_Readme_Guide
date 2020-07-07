c2d989ebba25acbd5bd5baa37b9dd7ad18366e21
Boaardz56 <jackie.lopez.56@gmail.com>
2020-07-07 05:59:49 -0400

    added to index.js and deleted previous license file

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
-var inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown")
+
+
 inquirer
 .prompt([
-
+ {
+     type: "input",
+     message: "What is your user name?",
+     name: "username"
+ },
+ {
+     type: ""
+ }
 ])
-.then(answers => {
+
+
+function generateMarkdown(answers) {
+    return `
+(#) ${answers.title}
+
+ ## Description
+
+${answers.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](##usage)
+* [License](##license)
+* [Contributors](##contributors)
+* [Tests](##tests)
     
-})
+## Installation
+
+${answers.install}
+
+## Usage 
+
+${answers.usage}
+
+##License
+
+${answers.license}
+
+## Contributors
+
+${answers.contributors}
+
+## Tests
+
+${answers.tests}
+`;
+}
+
+promptUser()
+    .then(function(answers) {
+        const readme = generateMarkdown(answers);
+
+        return 
+    })
