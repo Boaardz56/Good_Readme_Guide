abf93cbbabdb4b7bbd70790d164a04335ddccf2a
Boaardz56 <jackie.lopez.56@gmail.com>
2020-07-07 08:11:09 -0400

    updated questions

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
+const inquirer = require("inquirer");//so we can prompt user
+const fs = require("fs");//so we can file system then write
+const util = require("util");//so we have the promises
 const generateMarkdown = require("./utils/generateMarkdown")
 
 
-inquirer
-.prompt([
+function promptUser() {
+ return inquirer.prompt([
  {
      type: "input",
-     message: "What is your user name?",
-     name: "username"
+     message: "What is your project title?",
+     name: "title"
  },
  {
-     type: ""
- }
+     type: "input",
+     message: "What is your project about?",
+     name: "description"
+ },
+ {
+     type: "input",
+     message: "How do you install your project?",
+     name: "installation" 
+ },
+ {
+     type: "input",
+     message: "How do you use your project?",
+     name: "usage"
+ },
+ {
+     type: "input",
+     message: "Who contributed to this project?",
+     name: "contributors"
+}, 
+{
+    type: "input",
+    message: "Any tests you would like to run?",
+    name: "tests"
+},
 ])
-.then(answers => {
-    console.log(answers);
-})
+}
 
-.catch(error => {
-    if(error.isTtyError) {
-        prompt("Nothing rendered with this text");
-    }else {
-        prompt("Input text!");
-    }
-})
-
-const data = new Uint8Array(Buffer.from('Hello Node.js'));
-fs.writeFile('message.txt', data, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
-
-async function fn() {
-    return 'hello world';
-  }
-  const callbackFunction = util.callbackify(fn);
-  
-  callbackFunction((err, ret) => {
-    if (err) throw err;
-    console.log(ret);
-  });
 
 function generateMarkdown(answers) {
     return `
@@ -80,11 +77,36 @@ ${answers.tests}
 `;
 }
 
+//need to ask the questions
 promptUser()
-    .then(function(answers) {
-        const readme = generateMarkdown(answers);
-
-        return 
+   .then(function(answers) {
+       //building readme with template literal
+      const readme = generateMarkdown(answers);
+       // write a file with the readme
+      return writeFileAsync("README.md", readme);
     })
+   .then(function() {
+      console.log("successfully made readme file");
+    })
+   .catch(function(err) {
+      console.log(err);
+    });
 
 
+
+//============may use===================
+// const data = new Uint8Array(Buffer.from('Hello Node.js'));
+// fs.writeFile('message.txt', data, (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+
+// async function fn() {
+//     return 'hello world';
+//   }
+//   const callbackFunction = util.callbackify(fn);
+  
+//   callbackFunction((err, ret) => {
+//     if (err) throw err;
+//     console.log(ret);
+//   });
